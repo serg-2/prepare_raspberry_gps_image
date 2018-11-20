64ebbb4f9d0a0083c3a72b6c947cedc15d1f0865
serg-2 <31947289+serg-2@users.noreply.github.com>
2018-11-20 16:35:46 +0300

    Update burn_sdc.sh

--- burn_sdc.sh
+++ burn_sdc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo dd if=2018-10-09-raspbian-stretch-lite.img of=/dev/sdc bs=4M conv=fsync status=progress
+sudo dd if=raspbian-stretch-lite.img of=/dev/sdc bs=4M conv=fsync status=progress
